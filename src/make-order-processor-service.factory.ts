2aaf77d044dd180fd7e534f9825004d98582e5f0
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-30 18:43:49 -0300

    feat: add OrderProcessorService factory

---
+++ src/make-order-processor-service.factory.ts
@@ -0,0 +1,9 @@
+import { OrderProcessorService } from './services/order-processor/order-processor.service';
+import { FileReaderImpl } from './infra/file-reader';
+import { FileParserService } from './services/file-parser/file-parser.service';
+
+export const makeOrderProcessorService = (): OrderProcessorService => {
+  const fileReader = new FileReaderImpl();
+  const fileParser = new FileParserService();
+  return new OrderProcessorService(fileReader, fileParser);
+};
