2a2f519749882cd7b4f75ee7af3170fa3f0d0eb9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-30 16:02:43 -0300

    test: ensure Order adds multiple products

--- src/entities/order.spec.ts
+++ src/entities/order.spec.ts
@@ -16,4 +16,16 @@ describe('Order', () => {
     expect(order.products.length).toBe(1);
     expect(order.products[0]).toBe(product);
   });
+
+  test('handles multiple products', () => {
+    const product1 = new Product(1, '50.25');
+    const product2 = new Product(2, '25.75');
+
+    order.addProduct(product1);
+    order.addProduct(product2);
+
+    expect(order.products.length).toBe(2);
+    expect(order.products[0]).toBe(product1);
+    expect(order.products[1]).toBe(product2);
+  });
 });
