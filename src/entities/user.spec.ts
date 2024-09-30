7b895d15677d2eea95f1fee1d67fd7176d8c52ff
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-30 15:55:01 -0300

    test: ensure User handles multiple orders

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -16,4 +16,16 @@ describe('User', () => {
     expect(sut.orders.length).toBe(1);
     expect(sut.orders[0]).toBe(order);
   });
+
+  test('handle multiple orders', () => {
+    const order1 = new Order(100, '2022-01-01');
+    const order2 = new Order(101, '2022-02-01');
+
+    sut.addOrder(order1);
+    sut.addOrder(order2);
+
+    expect(sut.orders.length).toBe(2);
+    expect(sut.orders[0]).toBe(order1);
+    expect(sut.orders[1]).toBe(order2);
+  });
 });
