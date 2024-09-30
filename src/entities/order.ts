e3890d20f193335a5a0dae13cbfd830f3012a3f9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-30 15:25:50 -0300

    refactor: remove products and total properties from order constructor

--- src/entities/order.ts
+++ src/entities/order.ts
@@ -3,8 +3,6 @@ import { Product } from './product';
 export class Order {
   constructor(
     public order_id: number,
-    public total: string,
     public date: string,
-    public products: Product[],
   ) {}
 }
