3ddcab886a8e583b0784782b90e8c4ab4175f985
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-10-01 13:59:55 -0300

    refactor(order-processor-service): move users map to processFile method

--- src/services/order-processor/order-processor.service.ts
+++ src/services/order-processor/order-processor.service.ts
@@ -6,8 +6,6 @@ import { FileParserService } from '../file-parser/file-parser.service';
 import { User } from '../../entities/user';
 
 export class OrderProcessorService {
-  private users: Map<number, User> = new Map();
-
   constructor(
     private readonly fileReader: FileReader,
     private readonly fileParser: FileParserService,
@@ -39,16 +37,17 @@ export class OrderProcessorService {
   }
 
   private processFile(filePath: string): User[] {
+    const users: Map<number, User> = new Map();
     const lines = this.fileReader.read(filePath);
 
     lines.forEach((line) => {
       const { user, order, product } = this.fileParser.parseLine(line);
 
-      if (!this.users.has(user.user_id)) {
-        this.users.set(user.user_id, user);
+      if (!users.has(user.user_id)) {
+        users.set(user.user_id, user);
       }
 
-      const existingUser = this.users.get(user.user_id);
+      const existingUser = users.get(user.user_id);
       let existingOrder = existingUser.orders.find(
         (o) => o.order_id === order.order_id,
       );
@@ -61,6 +60,6 @@ export class OrderProcessorService {
       existingOrder.addProduct(product);
     });
 
-    return Array.from(this.users.values());
+    return Array.from(users.values());
   }
 }
